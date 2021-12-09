abfff916cd334e27b7c9a01b18c60a9793421c52
Mariano Patino-Paul <marianoppu@hotmail.com>
2021-12-09 11:31:11 -0500

    1

---
+++ Proj3Code/Proj3Code/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Proj3Code
+//
+//  Created by Mariano Patino-Paul on 12/9/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
